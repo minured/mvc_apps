621c3ec86d1a4ecb22bb6c3183107cedd151dcc4
minured <1296900813@qq.com>
2020-05-04 01:35:13 +0800

    add localStorage

--- src/app2.js
+++ src/app2.js
@@ -2,6 +2,8 @@ import $ from "jquery"
 import "./app2.css"
 const $tabBar = $("#app2 .tab-bar")
 const $tabContent = $("#app2 .tab-content")
+const index = localStorage.getItem("app2.index") || 0
+console.log(index)
 
 //jquery的事件委托
 $tabBar.on("click", "li", (e) => {
@@ -11,10 +13,11 @@ $tabBar.on("click", "li", (e) => {
         .siblings().removeClass("selected")
 
     const index = $li.index()   //找到第几个 从0开始
+    localStorage.setItem("app2.index", index)
     $tabContent
         //不要直接操作css，只操作类
         .children().eq(index).addClass("active")
         .siblings().removeClass("active")
 })
 //刚进页面的时候，模拟点一下
-$tabBar.children().eq(0).trigger("click")
+$tabBar.children().eq(index).trigger("click")
--- src/app3.js
+++ src/app3.js
@@ -1,8 +1,24 @@
 import "./app3.css"
 import $ from "jquery"
+const active = localStorage.getItem("app3.active") === "yes"
 
 const $square = $("#app3 .square")
+// if (active) {
+//     $square.addClass("active")
+
+// } else {
+//     $square.removeClass("active")
+// }
+
+$square.toggleClass("active", active)
 
 $square.on("click", () => {
-    $square.toggleClass("active")
+    if ($square.hasClass("active")) {
+        $square.removeClass("active")
+        localStorage.setItem("app3.active", "no")
+    } else {
+        $square.addClass("active")
+        localStorage.setItem("app3.active", "yes")
+    }
+    // $square.toggleClass("active")
 })
